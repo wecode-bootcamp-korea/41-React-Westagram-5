d21e8723ba445f5be01238f521d8abab098fe7cd
이진경 <ijinkyung12@gmail.com>
2022-12-21 15:18:54 +0900

    ADD : 초기세팅

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/styles/common.scss';
+import '../src/styles/reset.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>5팀 화이팅</h1>
+  </React.StrictMode>
+);
