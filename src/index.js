6d5000705423bb2aa277431e8c9346f6979dadc9
이진경 <ijinkyung12@gmail.com>
2022-12-21 15:26:04 +0900

    Modify: 초기세팅완료

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Router from './Router';
 import '../src/styles/common.scss';
 import '../src/styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>5팀 화이팅</h1>
-  </React.StrictMode>
-);
+root.render(<Router />);
