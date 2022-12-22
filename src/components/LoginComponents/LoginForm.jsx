e1d191a79de6fcdfc7c09c13da0a9f105b87a012
jakesungjaekim <codejakecarry@gmail.com>
2022-12-22 21:44:24 +0900

    ADD: Mission1 사용자 입력 데이터 저장 기능 구현 , Mission2 로그인 버튼 활성화 기능 구현(Active방법x-Modify필요)

--- src/components/LoginComponents/LoginForm.jsx
+++ src/components/LoginComponents/LoginForm.jsx
@@ -1,24 +1,78 @@
-import { useNavigate } from 'react-router-dom';
+import { useRef, useState } from 'react'
 
 // import faecbookImg from "../../Assets/images/facebook.png";
-import faecbookImg from '../../assets/images/sungjae/images/facebook.png';
+import faecbookImg from '../../assets/images/sungjae/images/facebook.png'
 
 const LoginForm = () => {
-  const navigate = useNavigate();
-  const goToMain = () => {
-    navigate('./main');
-  };
+  //로그인 input 함수 정리 {useState / useStateHandler }
+  const [state, setState] = useState({
+    identify: '',
+    password: '',
+  })
+
+  const changeStateHandler = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+  //로그인 button 함수 정리 { useRef+focus() }
+  const identifyInput = useRef()
+  const passwordInput = useRef()
+
+  const submitHandler = () => {
+    if (state.identify.length < 5 || !state.identify.includes('@')) {
+      identifyInput.current.focus()
+      return
+    }
+    if (state.password.length < 5) {
+      passwordInput.current.focus()
+      return
+    }
+
+    alert('로그인 성공!')
+  }
+
+  // focus() 2 조건이 맞춰줬을 떄 버튼 백그라운드 변경
+  const [color, setColor] = useState('')
+
+  const changeColorHandler = () => {
+    if (
+      (state.identify.length >= 6 || !state.identify.includes('@')) &&
+      state.password.length >= 6
+    )
+      setColor('#0074cc')
+  }
 
   return (
     <div className="form_wrap">
       <input
+        name="identify"
+        type="text"
         className="input"
         placeholder="Phone Number, username, or email"
-        type="text"
+        value={state.identify}
+        onChange={changeStateHandler}
+        ref={identifyInput}
       />
-      <input className="input" placeholder="Password" type="password" />
-      <button className="button" onClick={goToMain}>
-        Log in{' '}
+      <input
+        name="password"
+        type="password"
+        className="input"
+        placeholder="Password"
+        value={state.password}
+        onChange={changeStateHandler}
+        ref={passwordInput}
+      />
+      <button
+        style={{ backgroundColor: `${color}` }}
+        className="button"
+        onClick={() => {
+          submitHandler()
+          changeColorHandler()
+        }}
+      >
+        Log in
       </button>
 
       <div className="lineWrap">
@@ -34,7 +88,7 @@ const LoginForm = () => {
         <p> Forgot password?</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default LoginForm
--- src/pages/sungjae/Login/LoginMain.jsx
+++ src/pages/sungjae/Login/LoginMain.jsx
@@ -1,7 +1,8 @@
 // useNavigate
+import { useNavigate } from 'react-router-dom'
 
 // Img 가져오기
-import instagramLogo from '../../../assets/images/sungjae/images/instagramlogo.png';
+import instagramLogo from '../../../assets/images/sungjae/images/instagramlogo.png'
 
 // // Component 가져오기
 // import LoginForm from '../../Components/LoginComponents/LoginForm';
@@ -9,17 +10,21 @@ import instagramLogo from '../../../assets/images/sungjae/images/instagramlogo.p
 // import LoginAdvertisemnet from '../../Components/LoginComponents/LoginAdvertisement';
 
 // Compo 경로 수정
-import LoginForm from '../../../components/LoginComponents/LoginForm';
-import LoginSignup from '../../../components/LoginComponents/LoginSingup';
-import LoginAdvertisemnet from '../../../components/LoginComponents/LoginAdvertisement';
+import LoginForm from '../../../components/LoginComponents/LoginForm'
+import LoginSignup from '../../../components/LoginComponents/LoginSingup'
+import LoginAdvertisemnet from '../../../components/LoginComponents/LoginAdvertisement'
 
 const LoginMain = () => {
+  const navigate = useNavigate()
+  const goToMain = () => {
+    navigate('./main-sungjae')
+  }
   return (
     <>
       <div className="login_main">
         <div className="login_main_form">
           <div className="login_main_form_image">
-            <img src={instagramLogo} alt="instagramlogo" />
+            <img src={instagramLogo} alt="instagramlogo" onClick={goToMain} />
           </div>
           <LoginForm />
         </div>
@@ -27,7 +32,7 @@ const LoginMain = () => {
         <LoginAdvertisemnet />
       </div>
     </>
-  );
-};
+  )
+}
 
-export default LoginMain;
+export default LoginMain
